53af419843492c6a904ffe7d11643b1a5fdd3238
tjellicoe-tpzuk <149666043+tjellicoe-tpzuk@users.noreply.github.com>
2023-11-13 17:34:56 +0000

    Add files via upload
    Added functionality to export STAC catalogue alongside png

--- convert_image/__main__.py
+++ convert_image/__main__.py
@@ -9,6 +9,9 @@ from io import BytesIO
 import PIL.ImageOps
 import os
 import json
+import time
+import datetime as dt
+import mimetypes
 
 ## function to determine function to be done, here only resize or invert
 def do_func(args): # input is --fn invert --url "url" --size "ss"
@@ -20,6 +23,7 @@ def do_func(args): # input is --fn invert --url "url" --size "ss"
     elif func == "invert":
         ##call invert func
         invert_get_type(args)
+        #convert_get_type(args)
     else:
         raise Exception('This function is not supported in current implementation', func)
 
@@ -46,13 +50,19 @@ def invert_get_type(args):
     srcType = args[2]
     if srcType == "--url":
         #invert_url(args)
-        None
+        f = open(f"invert {srcType}.png", "w")
+        #f.write("hello world")
+        f.close()
     elif srcType == "--stac":
         #invert_stac(args)
-        None
+        f = open("invert {srcType}.png", "w")
+        #f.write("hello world")
+        f.close()
     elif srcType == "--file":
         #invert_file(args)
-        None
+        f = open("invert {srcType}.png", "w")
+        #f.write("hello world")
+        f.close()
     else:
         raise Exception()
 
@@ -68,6 +78,8 @@ def convert_url(url_name:str, size:str):
         out_size_tuple = size_to_tuple_url(url_name, out_size)
         out_im = im.resize(out_size_tuple)
         out_im.save(outName)
+    createStacItem(outName.replace(".png", ""))
+    createStacCatelogRoot(outName.replace(".png", ""))
 
 def convert_stac(file_dir:str, size:str):
 
@@ -105,6 +117,8 @@ def convert_stac(file_dir:str, size:str):
         out_im = im.resize(out_size_tuple)
         print("here " + outName)
         out_im.save(outName)
+    createStacItem(outName.replace(".png", ""))
+    createStacCatelogRoot(outName.replace(".png", ""))
 
 def convert_file(file_name:str, size:str):
     print("HERE")
@@ -119,6 +133,8 @@ def convert_file(file_name:str, size:str):
         print("here " + outName)
         #outName = "outimage.png"
         out_im.save(outName)
+    createStacItem(outName.replace(".png", ""))
+    createStacCatelogRoot(outName.replace(".png", ""))
 
 def invert_file(file_name:str, size:str):
 
@@ -127,8 +143,80 @@ def invert_file(file_name:str, size:str):
         out_im = PIL.ImageOps.invert(im)
     return out_im
 
-## Convert input size string to a double - removing percentage
 
+## Need to remove ".png" from end of outName
+def createStacItem(outName) :
+    now = time.time_ns()/1_000_000_000
+    dateNow = dt.datetime.fromtimestamp(now)
+    dateNow = dateNow.strftime('%Y-%m-%dT%H:%M:%S.%f') + "Z"
+    size = os.path.getsize(f"{outName}.png")
+    mime = mimetypes.guess_type(f"{outName}.png")[0]
+    data = {"stac_version": "1.0.0",
+  "id": f"{outName}-{now}",
+  "type": "Feature",
+  "geometry": {
+    "type": "Polygon",
+    "coordinates": [
+      [
+        [-180, -90],
+        [-180, 90],
+        [180, 90],
+        [180, -90],
+        [-180, -90]
+      ]
+    ]
+  },
+  "properties": {
+    "created": f"{dateNow}",
+    "datetime": f"{dateNow}",
+    "updated": f"{dateNow}"
+  },
+  "bbox": [-180, -90, 180, 90],
+  "assets": {
+    f"{outName}": {
+      "type": f"{mime}",
+      "roles": ["data"],
+      "href": f"{outName}.png",
+      "file:size": size
+    }
+    },
+  "links": [{
+    "type": "application/json",
+    "rel": "parent",
+    "href": "catalog.json"
+  }, {
+    "type": "application/geo+json",
+    "rel": "self",
+    "href": f"{outName}.png"
+  }, {
+    "type": "application/json",
+    "rel": "root",
+    "href": "catalog.json"
+  }]
+}
+    with open(f'{outName}.json', 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
+
+def createStacCatelogRoot(outName) :
+    data = {
+  "stac_version": "1.0.0",
+  "id": "catalog",
+  "type": "Catalog",
+  "description": "Root catalog",
+  "links": [{
+    "type": "application/geo+json",
+    "rel": "item",
+    "href": f"{outName}.json"
+  }, {
+    "type": "application/json",
+    "rel": "self",
+    "href": "catalog.json"
+  }]
+}
+    with open('catelog.json', 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
+
+## Convert input size string to a double - removing percentage
 def percent_to_num(percentage:str):
     
     if percentage.find("%") == -1:
@@ -165,16 +253,6 @@ def size_to_tuple_url(url_name:str, size:float):
 
     return new_size
 
-
-def create_output_stac():
-    create_stac_item()
-    create_stac_catelog_route()
-
-def create_stac_item():
-    None
-
-def create_stac_catelog_route():
-    None
 
 def convert_main(args: [str]):
     if len(args) == 1:
